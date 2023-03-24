e7b8ce8e4d7e3212827d3f1e9b7a43b7e9670613
Paul John <rainson.work@gmail.com>
2023-03-24 16:57:16 +0300

    Modified: Fixed Variable name

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ if last_digit > 5:
     print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
 elif last_digit < 6 and last_digit != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, lastdigit))
+          .format(number, last_digit))
 else:
     print(f"Last digit of {number:d} is {last_digit:d} and is 0")
