317c217f47b6f181a09e1635397dca5033b518ce
Paul John <rainson.work@gmail.com>
2023-03-30 21:09:20 +0300

    Add a Square Class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# 0-square.py by Paul John
+"""Defines a square class"""
+
+
+class Square(object):
+    """Represents a Square Class"""
+
+    def __init__(self, size=None):
+        """Initialize Square Class
+        Args: size - size of the square defined
+        """
+
+        self.__size = size
