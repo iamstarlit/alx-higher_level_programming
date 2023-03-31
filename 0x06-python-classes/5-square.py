61bcc306bb89a7a1447ed40e5bc99ec273a18c25
Paul John <rainson.work@gmail.com>
2023-03-31 06:41:16 +0300

    Remove redultant code in __init__ method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -11,17 +11,9 @@ class Square(object):
         """Initializes this Square Class
         Args:
             size - size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is negative number
         """
 
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
