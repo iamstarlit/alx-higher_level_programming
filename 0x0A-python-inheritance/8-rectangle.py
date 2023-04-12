8e7594ba2c44eb28af3bc3f90596a4d7614cf7d2
Paul John <rainson.work@gmail.com>
2023-04-12 16:40:04 +0300

    Add Rectangle; sub class of BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module that contains a Rectangle Sub Class."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a Sub Class of BaseGeometry, Rectangle."""
+
+    def __init__(self, width, height):
+        """Initializes an instance of Rectangle sub class.
+
+        Args:
+            width (int): width of a rectangle
+            height (int): height of a rectangle
+
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__with = width
+        self.__height = height
