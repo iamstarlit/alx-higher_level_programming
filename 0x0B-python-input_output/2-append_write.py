0d0792c0be4735fca99710f5b2a4f04835e7976a
Paul John <rainson.work@gmail.com>
2023-04-15 21:30:35 +0300

    Modify docstring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,14 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to a file."""
+    """Appends a string to a file.
+
+    Args:
+        filename (str): file name parameter.
+        text (str): text to append to a file.
+
+    Returns:
+       int: number of characters written to a file.
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
