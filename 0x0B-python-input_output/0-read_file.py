7c80da3b04b7befb5f99565be69f92a0deaadfb1
Paul John <rainson.work@gmail.com>
2023-04-15 19:15:44 +0300

    Add a function to read a file.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0-read_file.py module.
+"""
+
+def read_file(filename=""):
+    """Reads a text file UTF8 and print to stdout.
+
+    Args:
+        filename: file name
+
+    Raises:
+        Exception: if file cannot be opened
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read())
