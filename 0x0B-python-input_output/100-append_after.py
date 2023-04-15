13c9e59f9a01f163f725762982c0b3ea843ae262
Paul John <rainson.work@gmail.com>
2023-04-16 00:54:51 +0300

    Modify append_after function

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -15,12 +15,20 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): string to insert text after.
         new_string (str): text to insert into a file.
     """
-    text = ""
+    with open(filename, mode='r', encoding="utf-8") as f:
+        lines = f.readlines()
 
-    with open(filename, mode="w+", encoding="utf-8") as f:
-        for line in f:
-            text += line
+        # Create an empty list to store modified lines
+        new_lines = []
+
+        # Iterate over the lines and modify them
+        for line in lines:
+            # Append the original lines to the new_list
+            new_lines.append(line)
+
             if search_string in line:
-                text += new_string
+                new_lines.append(new_string)
 
-        f.write(text)
+    # open file in write mode and write the modified lines
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.writelines(new_lines)
