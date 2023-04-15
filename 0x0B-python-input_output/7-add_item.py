4617f9e6c36eca7ec72e062598f40296248089f0
Paul John <rainson.work@gmail.com>
2023-04-15 22:20:51 +0300

    Add a program to read and write to and from a JSON file.

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+7-add_item.py
+
+Adds all arguments to a Python list, then
+saves them to a file.
+"""
+
+# import modules
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        args = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        args = []
+
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")
