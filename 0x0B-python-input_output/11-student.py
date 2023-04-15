49158e95a54cf16688918dfc07b4c1fd5e2a0372
Paul John <rainson.work@gmail.com>
2023-04-15 23:38:49 +0300

    Add reload_from_json method

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+11-student.py
+
+A module that defines a Student class.
+"""
+
+
+class Student(object):
+    """A class that represents a Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student object.
+
+        Args:
+            first_name (str): student's first name
+            last_name (str): student's last name
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrive dictionary representantion of Student class.
+
+        Args:
+            attrs (list: 'str'): attribute names
+        Returns:
+            dict: if attr is a list, return names, otherwise
+            return whole dictionary.
+        """
+        if (type(attrs) == list and all(type(item) == str for item in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of Student object.
+
+        Args:
+            json (dict): JSON string dictionary
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
