d2f22b4fcf9385c16bd016b1668903e98119a531
Paul John <rainson.work@gmail.com>
2023-04-23 02:48:07 +0300

    Oops

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -4,32 +4,39 @@
 
 /**
  * print_python_string - Prints string information
- *
  * @p: Python Object
- * Return: no return
+ *
+ * Description: This function prints information about a Python string object.
+ * It checks whether the object passed is indeed a string, prints its type
+ * (either compact ASCII, compact Unicode, or legacy string), its length, and its value.
  */
 void print_python_string(PyObject *p)
 {
     Py_ssize_t length;
-    const char *value;
+    char *value;
 
     printf("[.] string object info\n");
 
     if (PyUnicode_Check(p))
     {
-        if (PyUnicode_IS_COMPACT_ASCII(p))
+        if (PyUnicode_IS_ASCII(p))
+        {
             printf("  type: compact ascii\n");
+        }
         else
+        {
             printf("  type: compact unicode object\n");
+        }
+        length = PyUnicode_GET_LENGTH(p);
         value = PyUnicode_AsUTF8AndSize(p, &length);
         printf("  length: %ld\n", length);
         printf("  value: %s\n", value);
     }
     else if (PyBytes_Check(p))
     {
-        printf("  type: bytes object\n");
+        printf("  type: bytes\n");
+        length = PyBytes_GET_SIZE(p);
         value = PyBytes_AsString(p);
-        length = PyBytes_Size(p);
         printf("  length: %ld\n", length);
         printf("  value: %s\n", value);
     }
