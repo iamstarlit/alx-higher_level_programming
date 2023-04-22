ab73966ec523e87b1a8cf4001417f1f0c43d1a32
Paul John <rainson.work@gmail.com>
2023-04-23 02:43:05 +0300

    Goodnight \o/

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -3,41 +3,38 @@
 #include <Python.h>
 
 /**
- * print_python_string - Prints information about a Python string object
+ * print_python_string - Prints string information
  *
- * @p: Python object
- *
- * This function prints the type (either compact ASCII or compact Unicode),
- * length, and value of a Python string object.
+ * @p: Python Object
+ * Return: no return
  */
 void print_python_string(PyObject *p)
 {
-    long int str_len;
-
-    fflush(stdout);
+    Py_ssize_t length;
+    const char *value;
 
     printf("[.] string object info\n");
 
-    /* Check if object is a string */
-    if (strcmp(p->ob_type->tp_name, "str") != 0)
+    if (PyUnicode_Check(p))
+    {
+        if (PyUnicode_IS_COMPACT_ASCII(p))
+            printf("  type: compact ascii\n");
+        else
+            printf("  type: compact unicode object\n");
+        value = PyUnicode_AsUTF8AndSize(p, &length);
+        printf("  length: %ld\n", length);
+        printf("  value: %s\n", value);
+    }
+    else if (PyBytes_Check(p))
+    {
+        printf("  type: bytes object\n");
+        value = PyBytes_AsString(p);
+        length = PyBytes_Size(p);
+        printf("  length: %ld\n", length);
+        printf("  value: %s\n", value);
+    }
+    else
     {
         printf("  [ERROR] Invalid String Object\n");
-        return;
     }
-
-    /* Get the length of the string */
-    str_len = ((PyASCIIObject *)(p))->length;
-
-    /* Print the length and value of the string */
-    printf(" length: %ld\n", str_len);
-
-    /* Get a wide character string representation of the string */
-    wchar_t *wide_str = PyUnicode_AsWideCharString(p, &str_len);
-    if (wide_str == NULL)
-    {
-        printf("  [ERROR] Could not convert string to wide character string\n");
-        return;
-    }
-    printf(" value: %ls\n", wide_str);
-    PyMem_Free(wide_str);
 }
