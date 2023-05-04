46ee3e0ed84a4feddfdf379dae47749804c0f62e
Paul John <rainson.work@gmail.com>
2023-05-04 21:36:20 +0300

    Add `test_base.py`

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,27 +8,42 @@ Test file for the `base.py` module in the models package.
 import unittest
 from models.base import Base
 
-
 class TestBase(unittest.TestCase):
-    """Class to perform unit tests on Base class."""
 
-    def test_default_id(self):
-        """Test instantiation with default id."""
+    def test_no_id(self):
+        """
+        Test that a Base instance is created with an id of 1 when no id is passed.
+        """
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
-    def test_given_id(self):
-        """Test instantiation with given id."""
-        b2 = Base(12)
-        self.assertEqual(b2.id, 12)
+    def test_positive_id(self):
+        """
+        Test that a Base instance is created with the correct id when a positive integer is passed.
+        """
+        b2 = Base(2)
+        self.assertEqual(b2.id, 2)
 
     def test_negative_id(self):
-        """Test with negative id."""
+        """
+        Test that a ValueError is raised when a negative integer is passed as an id.
+        """
         with self.assertRaises(ValueError):
             b3 = Base(-3)
 
-    def test_invalid_id(self):
-        """Test with invalid id: float and string."""
+    def test_float_id(self):
+        """
+        Test that a TypeError is raised when a float is passed as an id.
+        """
         with self.assertRaises(TypeError):
-            b4 = Base(4.5)
-            b5 = Base("Five")
+            b4 = Base(1.2)
+
+    def test_string_id(self):
+        """
+        Test that a TypeError is raised when a string is passed as an id.
+        """
+        with self.assertRaises(TypeError):
+            b5 = Base("hello")
+
+if __name__ == '__main__':
+    unittest.main()
