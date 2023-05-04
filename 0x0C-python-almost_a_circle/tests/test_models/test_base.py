4564ff4bc273acc0d42163b53346d833728c620d
Paul John <rainson.work@gmail.com>
2023-05-04 15:41:47 +0300

    Add `test_base.py`

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,6 +12,23 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """Class to perform unit tests on Base class."""
 
-    def test_id(self):
+    def test_default_id(self):
+        """Test instantiation with default id."""
         b1 = Base()
-        self.assertEqual(print(b1.id), 1)
+        self.assertEqual(b1.id, 1)
+
+    def test_given_id(self):
+        """Test instantiation with given id."""
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+
+    def test_negative_id(self):
+        """Test with negative id."""
+        with self.assertRaises(ValueError):
+            b3 = Base(-3)
+
+    def test_invalid_id(self):
+        """Test with invalid id: float and string."""
+        with self.assertRaises(TypeError):
+            b4 = Base(4.5)
+            b5 = Base("Five")
