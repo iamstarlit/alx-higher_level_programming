e1c8487aee925c69e3ce11041a9ccdf45b71d10c
Paul John <rainson.work@gmail.com>
2023-05-20 21:15:18 +0300

    Fixed the CREATE clause

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- cretes root user with all privileges.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd'
 WITH GRANT OPTION;
 
