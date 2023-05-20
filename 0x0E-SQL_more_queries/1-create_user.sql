6d7fac34d77f05deba8bbee671cfd159ffd43465
Paul John <rainson.work@gmail.com>
2023-05-20 21:11:18 +0300

    Add ON clause

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- cretes root user with all privileges.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd'
 WITH GRANT OPTION;
 
