12da5e03e86313f42a42379b36cc5beaaa146a1d
Paul John <rainson.work@gmail.com>
2023-06-29 19:53:52 +0300

    Add `1-rectangle.js`

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Rectangle class definition
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// export Rectangle module
+module.exports = Rectangle;
