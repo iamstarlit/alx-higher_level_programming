42d02c43c40cf4153438edb11cb47309c3aad288
Paul John <rainson.work@gmail.com>
2023-06-30 22:05:07 +0300

    Modify script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to the server and follow redirects
-curl -sL -X PUT --data "user_id=98" "0.0.0.0:5000/catch_me" -o /dev/null
+curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
