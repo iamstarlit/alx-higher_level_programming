f728baaee48206331df8cda3709a865095f2f033
Paul John <rainson.work@gmail.com>
2023-06-30 21:28:56 +0300

    change the algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
+"""Write a function that finds a peak in a list of unsorted integers."""
+
 
 def find_peak(list_of_integers):
-    # Check if the list is empty
-    if not list_of_integers:
-        return None
-
-    # Get the size of the list
-    size = len(list_of_integers)
-
-    # Check the edge cases for the first and last elements
-    if size == 1 or list_of_integers[0] >= list_of_integers[1]:
-        return list_of_integers[0]
-    elif list_of_integers[size - 1] >= list_of_integers[size - 2]:
-        return list_of_integers[size - 1]
-
-    # Binary search for the peak
-    left = 1
-    right = size - 2
-    while left <= right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] >= list_of_integers[mid - 1] and
-        list_of_integers[mid] >= list_of_integers[mid + 1]:
-            return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid - 1]:
-            right = mid - 1
-        else:
-            left = mid + 1
-
+    """[summary]"""
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
     return None
