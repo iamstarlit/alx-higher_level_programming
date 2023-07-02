b6cb535a353a98f138c67f169743b6adc95b5004
Paul John <rainson.work@gmail.com>
2023-07-02 12:19:39 +0300

    Refactor code

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
+// script that concats 2 files
 
-const fs = require('fs');
-
-const sourceFile1 = process.argv[2];
-const sourceFile2 = process.argv[3];
-const destinationFile = process.argv[4];
-
-const fileContents1 = fs.readFileSync(sourceFile1, 'utf8').trim();
-const fileContents2 = fs.readFileSync(sourceFile2, 'utf8').trim();
-const concatenatedContents = fileContents1 + '\n' + fileContents2 + '\n';
-
-fs.writeFileSync(destinationFile, concatenatedContents);
+const args = process.argv.slice(2);
+const file = require('fs');
+const contentA = file.readFileSync('./' + args[0]);
+const contentB = file.readFileSync('./' + args[1]);
+file.writeFileSync('./' + args[2], contentA + contentB);
