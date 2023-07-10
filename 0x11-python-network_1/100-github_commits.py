8251fdf942ef75911d42086250bdb8ca37461d40
Paul John <rainson.work@gmail.com>
2023-07-10 19:25:40 +0300

    Add `100-github_commits.py`

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 """
 Python script that takes 2 arguments in order to solve this challenge.
-The first argument will be the repository name
-The second argument will be the owner name
+The first argument will be the repository name.
+The second argument will be the owner name.
 """
 
-from sys import argv
+import sys
 import requests
 
-
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: {} <repository> <owner>".format(sys.argv[0]))
+        sys.exit(1)
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    res = requests.get(url)
-    commits = res.json()
+    repository = sys.argv[1]
+    owner = sys.argv[2]
 
-    try:
-        for i in range(10):
-            print('{}: {}'.format(
-                commits[i].get('sha'),
-                commits[i].get('commit"'.get('author').get('name'))
-                ))
-    except IndexError:
-        pass
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repository)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print('{}: {}'.format(sha, author_name))
+    else:
+        print("Failed to retrieve commits. Status code:", response.status_code)
