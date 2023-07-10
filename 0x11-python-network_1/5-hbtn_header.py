6baf80682cf53828ed69a865c54bef39248ae03a
Paul John <rainson.work@gmail.com>
2023-07-10 17:48:57 +0300

    Add `5-hbtn_header.py`

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header
+using requests module.
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    res = requests.get(url)
+    print(res.headers['X-Request-Id'])
