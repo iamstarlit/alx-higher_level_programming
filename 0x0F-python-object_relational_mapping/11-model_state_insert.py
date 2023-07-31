07c9ed93c8e50b2e1614d89091b4785d99b4bf30
Paul John <rainson.work@gmail.com>
2023-07-31 23:05:04 +0300

    Add `11-model_state_insert.py`

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that adds the State object
+`Louisiana` to the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Check the number of command-line arguments
+    if len(argv) != 4:
+        print(f"Usage: {argv[0]} <mysql_username> "
+              "<mysql_password> <mysql_db_name>")
+        exit(1)
+
+    # Get MySQL username, password, and database name from command-line args
+    mysql_username, mysql_password, mysql_db_name = argv[1:4]
+
+    # Create engine and session
+    db_url = ("mysql+mysqldb://{}:{}@localhost:3306/{}"
+              .format(mysql_username, mysql_password, mysql_db_name)
+              )
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Add the State object 'Louisiana' to the database
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    # Print the id of the newly added State object
+    print
