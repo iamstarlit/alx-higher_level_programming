a81945d52ef91fd8646f2c856a9c4a61a13b3a35
Paul John <rainson.work@gmail.com>
2023-07-31 22:23:16 +0300

    Modify model_state.py`

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Define the Base class using declarative_base()
 Base = declarative_base()
 
+
 # Define the State class
 class State(Base):
     """Defines the State class."""
