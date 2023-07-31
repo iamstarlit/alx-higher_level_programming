a5652d49c73f6bdabe1e460a659ffdf2478ac4ba
Paul John <rainson.work@gmail.com>
2023-07-31 23:58:15 +0300

    Mofiy `100-relationship_states_cities.py`

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,36 +3,33 @@
 Creates the State "California" with the City "San Francisco" from a DB
 """
 import sys
-from relationship_state import Base, State
-from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
 
 if __name__ == '__main__':
-    # Check the number of command-line arguments
-    if len(argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <mysql_db_name>"
-              .format(argv[0]))
-        exit(1)
-
-    # Get MySQL username, password, and database name from command-line args
-    mysql_username, mysql_password, mysql_db_name = argv[1:4]
+    # Get MySQL username, password, and database name from command line args
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db_name = sys.argv[3]
 
     # Create engine and session
     db_url = ("mysql+mysqldb://{}:{}@localhost:3306/{}"
               .format(mysql_username, mysql_password, mysql_db_name)
               )
-    engine = create_engine(db_url)
+
+    engine = create_engine(db_url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
+    # Use a context manager for the session
     Session = sessionmaker(bind=engine)
-    session = Session()
+    with Session() as session:
+        newState = State(name='California')
+        newCity = City(name='San Francisco')
+        newState.cities.append(newCity)
 
-    newState = State(name='California')
-    newCity = City(name='San Francisco')
-    newState.cities.append(newCity)
-
-    session.add(newState)
-    session.add(newCity)
-    session.commit()
+        session.add(newState)
+        session.add(newCity)
+        session.commit()
